47b4e80eed80c7c62e99ff75c10add704e7a46c8
Nguyễn Thanh Chính <73381800+Citnut@users.noreply.github.com>
2022-08-03 19:53:04 +0700

    disable update cli

--- main/bootloader/updateCli.js
+++ main/bootloader/updateCli.js
@@ -4,18 +4,18 @@ const axios = require('axios');
 module.exports = {
 	des: 'Kiem tra va cap nhat kb2abot-cli',
 	fn: async () => {
-		const {data} = await axios.get(
-			'https://raw.githubusercontent.com/kb2abot/kb2abot-cli/main/package.json'
-		);
-		const {version} = data;
-		let currentVersion;
-		try {
-			currentVersion = await execShellCommand('kb2abot-cli -v');
-		} catch (e) {
-			currentVersion = e.message;
-		}
-		if (currentVersion != version) {
-			await execShellCommand('npm i kb2abot-cli@latest -g');
-		}
+		// const {data} = await axios.get(
+		// 	'https://raw.githubusercontent.com/kb2abot/kb2abot-cli/main/package.json'
+		// );
+		// const {version} = data;
+		// let currentVersion;
+		// try {
+		// 	currentVersion = await execShellCommand('kb2abot-cli -v');
+		// } catch (e) {
+		// 	currentVersion = e.message;
+		// }
+		// if (currentVersion != version) {
+		// 	await execShellCommand('npm i kb2abot-cli@latest -g');
+		// }
 	}
 };
